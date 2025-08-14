8f7bd942dd8e2669d8db940d26de1bfc86226701
Krishna Chauhan <krishnachauhan09875@gmail.com>
2025-08-14 21:57:15 +0530

    Error

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-portfolio/", // <-- add your repo name here
+  base: "/my-portfolio/", // <-- ensures assets load correctly on GitHub Pages
   server: {
     host: "::",
     port: 8080,
