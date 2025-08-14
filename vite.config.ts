e6087b5d2de1b3de5a51e5147dc471cf5d02faa3
Krishna Chauhan <krishnachauhan09875@gmail.com>
2025-08-14 21:16:19 +0530

    Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/my-portfolio/", // <-- add your repo name here
   server: {
     host: "::",
     port: 8080,
